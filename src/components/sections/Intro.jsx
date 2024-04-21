d198cb27344f3cd5441ed9aeb8fb79f6ecfca5f6
Alma Herrstrom <almaherrstrom@hotmail.com>
2024-04-21 22:31:48 +0200

    started intro section desktop

--- src/components/sections/Intro.jsx
+++ src/components/sections/Intro.jsx
@@ -10,22 +10,32 @@ export const Intro = () => {
   return (
     <div className="intro" id="intro">
       <div className="mobile-profpic">
-        <img
-          src="/profilepic-S.png"
-          alt="profile picture of alma"
-        />
+        <img src="/profilepic-S.png" alt="profile picture for mobile" />
       </div>
-      <h1 className="title">Frontend developer</h1>
-      <p className="intro-text">
-        With a background in financial audit and a new-found passion for web
-        development, I&apos;m currently in a fast-paced boot camp learning tools
-        like React, Node.js and Express, I&apos;m blending my attention to
-        detail and problem-solving skills to create user-friendly digital
-        experiences.
-      </p>
-      <a className="intro-link" onClick={scrollToProjectSection}>
-        See my projects <IoArrowDown className="intro-icon" />
-      </a>
+      <div className="desktop-parent">
+        <div className="section desktop-left">
+          <div className="intro-text-container">
+            <h1 className="title">Frontend developer</h1>
+            <p className="intro-text">
+              With a background in financial audit and a new-found passion for
+              web development, I&apos;m currently in a fast-paced boot camp
+              learning tools like React, Node.js and Express, I&apos;m blending
+              my attention to detail and problem-solving skills to create
+              user-friendly digital experiences.
+            </p>
+          </div>
+
+          <a className="intro-link" onClick={scrollToProjectSection}>
+            See my projects <IoArrowDown className="intro-icon" />
+          </a>
+        </div>
+        <div className="section desktop-middle">
+          <img src="/profilepic-L.png" alt="profile picture for desktop" />
+        </div>
+        <div className="section desktop-right">
+          This is supposed to be on the right side
+        </div>
+      </div>
     </div>
   )
 }
