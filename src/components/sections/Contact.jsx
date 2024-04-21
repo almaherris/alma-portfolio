9f0d41dc897b2c1d8e094822b660d3917d838239
Alma Herrstrom <almaherrstrom@hotmail.com>
2024-04-21 14:05:51 +0200

    responsiveness tablet project section

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,7 +1,13 @@
+import { FaArrowUp } from "react-icons/fa";
 import { SoMe } from "../reusable/SoMe"
 import "./Contact.css"
 
 export const Contact = () => {
+  const scrollToIntro = () => {
+    const projectSection = document.getElementById("intro")
+    projectSection.scrollIntoView({ behavior: "smooth" })
+  }
+
   return (
     <div className="contact" id="contact">
       <h2 className="title">Contact</h2>
@@ -18,6 +24,11 @@ export const Contact = () => {
 
       <div className="some-icons">
         <SoMe />
+      </div>
+      <div className="up-btn-container">
+        <button className="up-button" onClick={scrollToIntro}>
+        <FaArrowUp />
+        </button>
       </div>
     </div>
   )
