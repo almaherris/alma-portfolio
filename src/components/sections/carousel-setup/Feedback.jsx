b4314e8789a0116b156914f8e1f1268f51c41882
Alma Herrstrom <almaherrstrom@hotmail.com>
2024-05-03 10:53:40 +0200

    new cv and changed title for feedback section

--- src/components/sections/carousel-setup/Feedback.jsx
+++ src/components/sections/carousel-setup/Feedback.jsx
@@ -3,7 +3,7 @@ import { CarouselComponent } from "./CarouselComponent"
 export const Feedback = () => {
   return (
     <div className="feedback" id="feedback">
-      <h2 className="title">Words about me</h2>
+      <h2 className="title">Testimonials</h2>
       <div className="feedback-container">
         <CarouselComponent />
       </div>
